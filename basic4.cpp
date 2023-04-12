0745455edd074aadabda819f3cdc31bcbb610004
The-AdityaJain <124339556+The-AdityaJain@users.noreply.github.com>
2023-04-12 23:53:00 +0530

    Update basic4.cpp

--- basic4.cpp
+++ basic4.cpp
@@ -2,25 +2,25 @@
 #include<string>
 using namespace std;
 
-class student_info
-{
+class student_info{
+	
 	public:
 	string name;
 	int roll_no;
 	
-	void put_data()
-	{
+	void put_data(){
+		
 		cout<<"Enter Name"<<endl;
 		cin>>name;
 		cout<<"Enter Roll No"<<endl;
 		cin>>roll_no;
 	}
 	void get_data(){
-		cout<<"Name : "<<name<<"\nRoll No. : "<<roll_no<<endl;
+		cout<<name<<"\t"<<roll_no;
 	}
 };
-class course
-{
+class course{
+	
 	public:
 	string cours;
 	void put_data(){
@@ -29,38 +29,45 @@ class course
 	
 	}
 	void get_data(){
-		cout<<"Course Name : "<<cours<<endl;
+		cout<<"\t"<<cours;
 	}
 };
-class dept:public student_info,public course
-{
+class dept:public student_info,public course{
+	
 	public:
 	string dep;
 	void put_data();
 	void get_data();
 };
-	void dept::put_data()
-	{
+	void dept::put_data(){
+		
 		student_info::put_data();
 		course::put_data();
 		cout<<"Enter your branch"<<endl;
 		cin>>dep;
 	}
-	void dept::get_data()
-	{
+	void dept::get_data(){
+		
 		student_info::get_data();
 		course::get_data();
-		cout<<"Branch : "<<dep<<endl;
-	}
+		cout<<"\t"<<dep<<endl;
+    }
+    void display(){
+        cout<<"Name"<<"\t"<<"Roll No"<<"\t"<<"Course"<<"\t"<<"Dept"<<endl;
+    }
 int main(){
+
 	dept d1;
 	d1.put_data();
-	d1.get_data();
 	dept d2;
 	d2.put_data();
-	d2.get_data();
 	dept d3;
 	d3.put_data();
+
+        display();
+        d1.get_data();
+        d2.get_data();
 	d3.get_data();
+        
 return 0;
 }
