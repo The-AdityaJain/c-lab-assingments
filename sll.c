da8e4c114a6c09dbcc6beba7ed3dbc4a0e17ea1b
Aditya Jain <124339556+the-adityajain@users.noreply.github.com>
2023-11-30 22:26:44 +0530

    sll.c

--- sll.c
+++ sll.c
@@ -6,15 +6,6 @@ struct node{
 	char name[20];
 	struct node *next;
 };
-int main(){
-	struct node *head;
-	head=(struct node *)malloc(sizeof(struct node));
-	head->next=NULL;
-	create(head);
-	display(head);
-	int size=length(head);
-	printf("Length is %d",length);
-}
 void create(struct node *head){
 	struct node *temp,*curr;
 	temp=head;
@@ -29,7 +20,7 @@ void create(struct node *head){
 	temp->next=curr;
 	temp=curr;
 	printf("node added\n");
-	printf("Do you want to continue? Press 1 to continue");
+	printf("Do you want to continue? Press 1 to continue :");
 	scanf("%d",&y);
 	
 	}while(y==1);
@@ -42,20 +33,22 @@ void display(struct node *head){
 	}
 	else{
 		curr=head->next;
-	}
-	printf("PRN\tName\tPosition\n");
-	if(curr==head->next){
-		printf("%d\t%s\tPresident\n",curr->prn,curr->name);
-	}
-	curr=curr->next;
-	while(curr->next!=NULL){
-		printf("%d\t%s\tMember\n",curr->prn,curr->name);
-		curr=curr->next;
-	}
-	if(curr->next==NULL){
-		printf("%d\t%s\tSecretory\n",curr->prn,curr->name);	
-			
-	}
+        printf("PRN\tName\tPosition\n");
+        while(curr!=NULL){
+            if(curr==head->next){
+                printf("%d\t%s\tPresident\n",curr->prn,curr->name);
+                curr=curr->next;
+            }
+            else if(curr->next!=NULL){
+                printf("%d\t%s\tMember\n",curr->prn,curr->name);
+                curr=curr->next;
+            }
+            else{
+                printf("%d\t%s\tSecretory\n",curr->prn,curr->name);	
+                curr=curr->next;      
+            }
+        }
+    }
 
 
 }
@@ -74,15 +67,38 @@ int length(struct node *head){
 	}
 	return i;
 }
-void insertbypos(*head){
-	int i = 1;
-	curr=head;
+void insertbypos(struct node *head){
+    
+    struct node *temp,*curr,*nnode;
+	int i = 1,pos;
+	curr=head->next;
 	nnode = (struct node*)malloc(sizeof(struct node));
 	printf("Enter the prn in node : ");
 	scanf("%d",&nnode->prn);
 	printf("Enter the name in node : ");
 	scanf("%s",&nnode->name);
-	printf("Enter the position of new node to be inserted");
-	scanf("%d",&nnode->pos);
+	printf("Enter the position of new node to be inserted : ");
+	scanf("%d",&pos);
+    if(pos>(length(head)+1)){
+        printf("error!!!");
+    }
+    while(curr!=NULL && i<pos){
+        curr=curr->next;
+        i++;
+    }
+    nnode->next=curr->next;
+    curr->next=nnode;
 
+}
+int main(){
+	struct node *head;
+	head=(struct node *)malloc(sizeof(struct node));
+	head->next=NULL;
+	create(head);
+	display(head);
+	int size=length(head);
+	printf("Length is %d\n",size);
+    insertbypos(head);
+    
+    display(head);
 }
