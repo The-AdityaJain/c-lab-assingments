c43abbe6b08a46cacad8735a3f9a59969c97528f
Aditya Jain <124339556+the-adityajain@users.noreply.github.com>
2023-12-03 17:12:02 +0530

    stack-evalutaion.c

--- stack-evalutaion.c
+++ stack-evalutaion.c
@@ -1,122 +1,75 @@
 #include <stdio.h>
-#include<ctype.h>
+#include <ctype.h>
 
+#define SIZE 50
 
-#define MAX 5
+int stack[SIZE];
 int top = -1;
-int stk[MAX];
 
-
-int isFull() {
-    if (top == MAX - 1) {
-        return 1;
+void pushval(int value) {
+    if (top == SIZE - 1) {
+        printf("Stack Overflow\n");
+        return;
     }
-    else {
-       return 0;
-    }
+    stack[++top] = value;
 }
-int isEmpty() {
+
+int popval() {
     if (top == -1) {
-        return 1;
+        printf("Stack Underflow\n");
+        return -1;  // Assuming -1 as an error value
     }
-    else {
-        return 0;
-    }
+    return stack[top--];
 }
-void push(int item){
-	if(!isFull()){
-		top=top+1;
-		stk[top]=item;
-	}
-	else{
-		printf("STACK IS FULL\n");
-	}
-}
-int pop(){
-        int temp;
-        if(!isEmpty()){
-		temp=stk[top];
-		top=top-1;
-		return temp;
-	}
-	else{
-		printf("STACK IS EMPTY\n");
-		return 0;
-	}
-}
-int main(){
-	int item;
-	int choice;
-    
-	do{  
-        	printf("\nPerform operations on the stack :");  
-        	printf("\n1.Push the element\n2.Pop the element\n3.End");  
-        	printf("\n\nEnter the choice : ");  
-        	scanf("%d", &choice);  
-        	switch (choice){  
-        		case 1:  
-        			printf("Enter item to push : ");
-        			scanf(" %c",&item);
-            			push(item);  
-            			break;  
-        		case 2:  
-            			item = pop();  
-            			printf("popped item is %c",item);
-            			break;  
-            		case 3:
-            			break;
-        	}  
-    	}while(choice!=3);  
-    	int inexp[20],postexp[20];
-    	printf("Enter the string : ");
-    	scanf("%s",&postexp);
-        eval(postexp);
-        
-        
 
-    	
-}
-void eval(char post[MAX]){
-     int op1,op2,ans,z;
-    for(int i=0;post[i]!='\0';i++){
-        
-        if(isalpha(post[i])!=0) {
-       
-        printf("\nEnter value of %c",post[i]);
-        scanf("%d",&z);
-        push(z);
-        }
-        else{
-        op1=pop();
-        op2=pop();
-        ans=calc(op1,op2,post[i]);
-        push(ans);
-        }
-    } 
-    printf("\nEvaluation is. ");
-    printf("%d",stk[top]);
-    
-}
-int calc(int a,int b,int op){
+int calc(int a, int b, char op) {
     int ans;
-    switch(op){
-
+    switch(op) {
         case '+':
-        ans=a+b;
-        break;
+            ans = a + b;
+            break;
         case '-':
-        ans-a-b;
-        break;
+            ans = a - b;
+            break;
         case '*':
-        ans=a*b;
-        break;
+            ans = a * b;
+            break;
         case '/':
-        ans = a / b;
-        break;
-        default:;
-
+            ans = a / b;
+            break;
+        default:
+            printf("Invalid operator\n");
+            ans = 0;  // Assuming 0 as an error value
     }
-    return(ans);
+    return ans;
 }
 
+void eval(char post[SIZE]) {
+    int i, z, op1, op2, ans;
 
+    for (i = 0; post[i] != '\0'; i++) {
+        if (isalpha(post[i]) != 0) {
+            printf("\nEnter value of %c: ", post[i]);
+            scanf("%d", &z);
+            pushval(z);
+        } else {
+            op1 = popval();
+            op2 = popval();
+            ans = calc(op1, op2, post[i]);
+            pushval(ans);
+        }
+    }
+
+    printf("\nEvaluation is: %d\n", stack[top]);
+}
+
+int main() {
+    char postfixExpression[SIZE];
+
+    printf("Enter postfix expression: ");
+    scanf("%s", postfixExpression);
+
+    eval(postfixExpression);
+
+    return 0;
+}
